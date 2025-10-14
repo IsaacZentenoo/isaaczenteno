67506b2f4f19f169a105ec991d8deeb08e943f4f
IsaacZentenoo <isaaczentones@gmail.com>
2025-10-13 18:59:09 -0600

    exercise06

--- Portfolio/06-PersonalWebsite(JS)/js/server.js
+++ Portfolio/06-PersonalWebsite(JS)/js/server.js
@@ -1,1 +1,38 @@
-console.log("Running the script");
+document.addEventListener("DOMContentLoaded", function() {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+var toastEl = document.querySelector('.toast');
+  if (toastEl) {
+    var toast = new bootstrap.Toast(toastEl);
+    toast.show();
+}
+var scheduleForm = document.getElementById("scheduleForm");
+var scheduleTable = document.getElementById("scheduleTable") 
+? document.getElementById("scheduleTable").querySelector("tbody")
+: null;
+
+scheduleForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    var subject = document.getElementById("subject").value;
+    var date = document.getElementById("date").value;
+    var startTime = document.getElementById("startTime").value;
+    var endTime = document.getElementById("endTime").value;
+    var location = document.getElementById("location").value;
+
+if (scheduleTable) {
+    var newRow = document.createElement("tr");
+    newRow.innerHTML = `
+    <td>${subject}</td>
+    <td>${date}</td>
+    <td>${startTime}</td>
+    <td>${endTime}</td>
+    <td>${location}</td>`;
+    scheduleTable.appendChild(newRow);
+    }
+    var modal = new bootstrap.Modal(document.getElementById("scheduleModal"));
+    modal.show();
+    scheduleForm.reset();
+  });
+});
